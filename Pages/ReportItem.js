a04fe9e23844630d67ffd1c304d6a24e31618f40
umair-gititall <rashid.summit2@gmail.com>
2025-06-07 13:06:43 +0000

    Removed AutoComplete + Changed Animation Style: ReportItem

--- Pages/ReportItem.js
+++ Pages/ReportItem.js
@@ -67,3 +67,12 @@
 
     input.value = "";
   });
+
+
+const words = ["Loading.", "Loading..", "Loading..."];
+let index = 0;
+
+setInterval(() => {
+  document.getElementById("loader").textContent = words[index];
+  index = (index + 1) % words.length;
+}, 200);
