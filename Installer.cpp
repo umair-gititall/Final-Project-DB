e44385473bea73073285aedebdabd5a4575c218a
umair-gititall <rashid.summit2@gmail.com>
2025-08-15 15:41:30 +0000

    Installer Script

---
+++ Installer.cpp
@@ -0,0 +1,167 @@
+#include "UI.h"
+#include <cstdlib>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Installer {
+  ConsoleUI ui;
+  string installer;
+  string *steps;
+
+  void GetTools() {
+    do {
+      ui.title();
+      ui.fancyMenu(steps, 1, false, "Steps");
+      ui.loading(steps[1]);
+
+      if (installer == "apt")
+        system("sudo apt-get update && sudo sudo "
+               "apt-get "
+               "install git apache2 nodejs npm mysql-server php php-mysqli -y");
+      else if (installer == "pacman")
+        system("sudo pacman -S --noconfirm git "
+               "apache nodejs npm mysql php php-mysqli");
+      else if (installer == "yum")
+        system("sudo yum update -y && sudo yum install git httpd nodejs npm "
+               "mysql-server php php-mysqli -y");
+      else if (installer == "dnf")
+        system("sudo dnf upgrade -y && sudo dnf install git httpd nodejs npm "
+               "mysql-server php php-mysqli -y");
+      system("clear");
+
+      // if (!(system("git -v") || system("node -v") || system("npm -v") ||
+      //       system("mysql -v") || system("php -v") ||
+      //       system("php -m | grep -i mysqli")))
+      //   if (system("apache2 -v") == 0 || system("apache -v") == 0 ||
+      //       system("httpd -v") == 0)
+      //     break;
+      if (system("git -v") == 0 && system("node -v") == 0 &&
+          system("npm -v") == 0 && system("mysql --version") == 0 &&
+          system("php -v") == 0 && system("php -m | grep -i mysqli") == 0)
+        if (system("apache2 -v") == 0 || system("apache -v") == 0 ||
+            system("httpd -v") == 0)
+          break;
+      system("clear");
+    } while (true);
+
+    steps[1] = "✅  " + steps[1];
+    GetFiles();
+  }
+
+  void GetFiles() {
+    ui.title();
+    ui.fancyMenu(steps, 2, false, "Steps");
+
+    do {
+      ui.loading(steps[2]);
+    } while (
+        system(
+            "sudo git clone "
+            "https://github.com/umair-gititall/Final-Project-DB.git "
+            "/tmp/fpdb && sudo cp -r /tmp/fpdb/* /var/www/html/ && sudo rm -rf "
+            "/tmp/fpdb") != 0);
+
+    steps[2] = "✅  " + steps[2];
+    SetDatabase();
+  }
+
+  void SetDatabase() {
+    ui.title();
+    ui.fancyMenu(steps, 3, false, "Steps");
+
+    system("sudo systemctl enable mysql");
+    system("sudo systemctl start mysql");
+
+    do {
+      ui.loading(steps[3]);
+    } while (system("mysql -u root -p -e \"SOURCE "
+                    "/var/www/html/Requirements/Database.sql; "
+                    "CREATE USER 'LostFoundSystem'@'localhost' IDENTIFIED BY "
+                    "'LostFoundManagementSystem'; GRANT ALL PRIVILEGES ON "
+                    "LostFoundDB.* TO "
+                    "'LostFoundSystem'@'localhost'; FLUSH PRIVILEGES;\"") != 0);
+
+    steps[3] = "✅  " + steps[3];
+    SetApiKey();
+  }
+
+  void SetApiKey() {
+    string temp[] = {"Open https://aistudio.google.com/apikey",
+                     "Press Create API Key", "Press Copy", "Paste Here"};
+    string check = "";
+    fstream validator;
+
+    ui.title();
+    ui.fancyMenu(steps, 4, false, "Steps");
+
+    do {
+      ui.fancyMenu(temp, 4, true, "Gemini API Key");
+      getline(cin, installer);
+      system(("curl -s -o /dev/null -w \"%{http_code}\n\" "
+              "\"https://generativelanguage.googleapis.com/v1/models?key=" +
+              installer + "\" > /tmp/check.txt")
+                 .c_str());
+
+      validator.open("/tmp/check.txt");
+      validator >> check;
+
+      if (check == "200")
+        break;
+      ui.BoxedMessage("Invalid Api Key");
+    } while (true);
+
+    system("rm /tmp/check.txt");
+    validator.close();
+    validator.open("/var/www/html/Requirements/Ai/.env");
+    validator << "GOOGLE_API_KEY=" << installer;
+
+    validator.close();
+  }
+
+public:
+  Installer()
+      : steps(new string[6]{"Determining OS", "Getting Required Tools",
+                            "Getting Required Files", "Setting Database",
+                            "Setting Gemini Api Key"}) {
+    do {
+      ui.title();
+      ui.fancyMenu(steps, 0, true, "Start Installation (y/n)");
+      getline(cin, installer);
+
+      if (installer == "y" || installer == "Y") {
+        ui.loading(steps[0]);
+
+        if (system("apt -v") == 0)
+          installer = "apt";
+        else if (system("pacman -v") == 0)
+          installer = "pacman";
+        else if (system("yum -v") == 0)
+          installer = "yum";
+        else if (system("dnf -v") == 0)
+          installer = "dnf";
+        else
+          installer = "none";
+        break;
+      } else if (installer == "n" || installer == "N") {
+        installer = "none";
+        break;
+      }
+
+      system("clear");
+    } while (true);
+
+    steps[0] = "✅  " + steps[0];
+
+    if (installer != "none")
+      GetTools();
+    else
+      ui.BoxedMessage("OS not Supported!!");
+  }
+};
+
+int main() {
+  Installer Install;
+
+  return 0;
+}
---
+++ UI.h
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <sys/ioctl.h>
+#include <thread>
+#include <unistd.h>
+using namespace std;
+
+class ConsoleUI {
+  const string WHITE = "\033[97m";
+  const string RED = "\033[31m";
+  const string GREEN = "\033[32m";
+  const string YELLOW = "\033[33m";
+  const string CYAN = "\033[36m";
+  const string MAGENTA = "\033[95m";
+  const string GREY = "\033[90m";
+  const string ITALIC = "\033[3m";
+  const string RESET = "\033[0m";
+  const string TOP = "\033[1;1H";
+  const string SAVE = "\033[s";
+  const string JUMP = "\033[2;1H";
+
+  int width() {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    return w.ws_col;
+  }
+
+  // Loading Bar
+  void progressBar(int total = 20, int delayMS = 100) {
+    cout << YELLOW << "Progress: [" << GREEN;
+    for (int i = 0; i < 20; i++) {
+      cout << "#" << flush;
+      this_thread::sleep_for(chrono::milliseconds(delayMS));
+    }
+    cout << YELLOW << "] Done!" << RESET;
+    this_thread::sleep_for(chrono::milliseconds(delayMS + 50));
+  }
+
+  // Text Animations
+  void typingEffect(string message, int delayMS = 40) {
+    for (char c : message) {
+      cout << c << flush;
+      this_thread::sleep_for(chrono::milliseconds(delayMS));
+    }
+  }
+
+public:
+  // Headings
+  void title(string message = "Lost And Found Management System",
+             bool AllowColors = true) {
+    string spaces((width() - message.length()) / 2, ' ');
+    string line(message.length() + 10, '=');
+    int color = rand() % 5;
+
+    cout << spaces << CYAN << ITALIC << line << endl;
+
+    if (AllowColors) {
+      if (color == 0)
+        cout << GREY;
+      else if (color == 1)
+        cout << RED;
+      else if (color == 2)
+        cout << MAGENTA;
+      else if (color == 3)
+        cout << WHITE;
+      else if (color == 4)
+        cout << YELLOW;
+    }
+
+    cout << spaces << "     " << message << endl;
+    cout << spaces << CYAN << line << endl << RESET;
+  }
+
+  // Pop Ups
+  void BoxedMessage(string message, bool effect = false) {
+    string spaces((width() - message.length()) / 2, ' ');
+    string line(message.length() + 4, '=');
+
+    cout << spaces << GREEN << "  +" << line << "+\n" << spaces << "  |  ";
+    if (effect)
+      typingEffect(message);
+    else
+      cout << message;
+
+    cout << "  |\n" << spaces << "  +" << line << "+\n" << RESET;
+  }
+
+  // Printing Menu
+  void fancyMenu(string *options, int size, bool input,
+                 string heading = "Main Menu") {
+    string spaces((width() - heading.length()) / 2, ' ');
+
+    title(heading, false);
+    for (int i = 0; i < size; i++) {
+      cout << spaces << YELLOW << " [" << i + 1 << "] " << RESET;
+      typingEffect(options[i]);
+      cout << endl;
+    }
+    if (input)
+      cout << CYAN << " Root@Console ➤ " << RESET;
+  }
+
+  void loading(string message) {
+    system("clear");
+    title();
+    BoxedMessage(message);
+    string spaces((width() - 38) / 2, ' ');
+    cout << spaces;
+    progressBar();
+    cout << endl;
+    system("clear");
+  }
+
+  void pause() {
+    cout << "Enter to continue.....";
+    cin.get();
+  }
+};
